a52fc3f8db4697862b91a5d880ac2d45c49d1b13
Elizabeth Crouther <56413692+elleanne@users.noreply.github.com>
2021-08-13 16:35:16 -0400

    Add test classes

---
+++ Test.java
@@ -0,0 +1,12 @@
+
+/*
+ * Crawl the LOC Law Library for the user-inputed search words
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello from Test");
+        ConnectTest  ct = new ConnectTest();
+        System.out.println(ct.sayHi());
+    }
+
+}
